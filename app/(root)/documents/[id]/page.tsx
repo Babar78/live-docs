a5d8e142b67fe8310099b96f42d2f7e8fc8e175e
Babar78 <muhammadbabarashraf8@gmail.com>
2024-07-28 06:16:23 -0700

    chore: Authentication using Clerk and create sign-in and sign-up pages

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -2,6 +2,8 @@ import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 import React from "react";
 
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 const Document = () => {
   return (
     <div>
@@ -9,6 +11,12 @@ const Document = () => {
         <div className="flex w-fit items-center justify-center gap-2">
           <p className="document-title">Document Title</p>
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
